e7e7393576fc26e9e736347159409871f2ad908f
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2023-04-27 13:53:57 +0100

    Update following core renewal (#13)

--- Runtime/Interfaces/ISpatialPersistenceServiceModule.cs
+++ Runtime/Interfaces/ISpatialPersistenceServiceModule.cs
@@ -1,5 +1,4 @@
 // Copyright (c) Reality Collective. All rights reserved.
-// Copyright (c) XRTK. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using RealityCollective.ServiceFramework.Interfaces;
