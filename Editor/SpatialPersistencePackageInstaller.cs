a3f3b4dd20a9a903c39d4185a7fccc1fef53fd99
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2024-05-08 19:43:54 +0100

    Workflow update and package clean-up (#24)

--- Editor/SpatialPersistencePackageInstaller.cs
+++ Editor/SpatialPersistencePackageInstaller.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Reality Collective. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using RealityCollective.ServiceFramework;
 using RealityCollective.ServiceFramework.Editor;
 using RealityCollective.ServiceFramework.Editor.Packages;
 using RealityCollective.Utilities.Editor;
